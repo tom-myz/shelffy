f275c73dc9a36caf60cc4c2f84fd70616171400e
tom-myz <myztmm@gmail.com>
2016-12-14 20:31:57 +0900

    Add first commit

---
+++ src/main/java/moe/awawa/ShelffyApplication.java
@@ -0,0 +1,12 @@
+package moe.awawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelffyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelffyApplication.class, args);
+	}
+}
