36218c24e5e35c6e0744f27afb7028798554bf96
tom-myz <myztmm@gmail.com>
2019-11-29 16:56:05 +0900

    Add simple controller (#2)
    Test goJF
    
    * Add simple controller
    
    * [goJF]
    
    * Try useless commit
    
    * [goJF]
    
    * Specify remote branch
    
    * Fix
    
    * [goJF]

--- src/main/java/moe/awawa/ShelffyApplication.java
+++ src/main/java/moe/awawa/ShelffyApplication.java
@@ -2,8 +2,14 @@ package moe.awawa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@Configuration
+@EnableWebMvc
+@ComponentScan("moe.awawa.controller")
 public class ShelffyApplication {
 
   public static void main(String[] args) {
