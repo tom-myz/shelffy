c710930410f32b5544f91e30d006af64a0d6a297
tom-myz <tmiyazu@caulis.jp>
2019-11-29 16:39:54 +0900

    Add simple controller

--- src/main/java/moe/awawa/ShelffyApplication.java
+++ src/main/java/moe/awawa/ShelffyApplication.java
@@ -1,9 +1,16 @@
 package moe.awawa;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@Configuration
+@EnableWebMvc
+@ComponentScan("moe.awawa.controller")
 public class ShelffyApplication {
 
   public static void main(String[] args) {
